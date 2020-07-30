2828e84310dc3770b33d945d7beb110e0507a1f2
mir4ri <leobuchko@gmail.com>
2020-07-30 13:19:39 +0300

    fix schedule

---
+++ _next/static/webpack/styles.309b054b5ce1a142bf07.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("styles",{
+
+/***/ "./pages/schedule/components/Lecturer/lecturer.scss":
+/*!**********************************************************!*\
+  !*** ./pages/schedule/components/Lecturer/lecturer.scss ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"lecturer":"_2u9x82mfSJlJx09l5cp-SS","filter":"kFkaqRLQ3V-Zs_yD5v02Z","fadein":"_1XBCTWLbvu-_GLCSYYJtkq","departament":"_3ELyTuM2QzAMuBcb74aJlL","day":"_3hNXdTOdF4RkgCh3PyHpLN","header":"_1-wgK6vNMXXgAgFs3xowol","weekday":"_2-woqwW0CG004v_J0v6CeW","lesson":"_18LkyKq3MkT9s5xNOqVmFT","start":"_3SWDPfFoWnezWG-7ZQtqsC","groupAndAudience":"wN2reS-Clam-iKtRntIX_","group":"_456dTtIKe8J_QRE-TGvtV","students":"_1-frEucmXti058oEGrhm3L","specialty":"_2cdhxcQular-RsNtncd1a8","schedule":"_6ul5VqaiOlYNmNzTOKdHT","subjectAndLecturer":"_2HADccNRl4vXLJEaHCB_lL"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1596103991205");
+          });
+      }
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=styles.309b054b5ce1a142bf07.hot-update.js.map
---
+++ _next/static/webpack/styles.f59ad3fd659b4456c007.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate("styles",{
+
+/***/ "./components/News/news.scss":
+false,
+
+/***/ "./pages/index/index.scss":
+false,
+
+/***/ 7:
+false,
+
+/***/ 8:
+false
+
+})
+//# sourceMappingURL=styles.f59ad3fd659b4456c007.hot-update.js.map
