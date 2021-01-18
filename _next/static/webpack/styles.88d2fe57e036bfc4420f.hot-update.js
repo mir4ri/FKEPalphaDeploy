b98f2eb92b4bedb879f2804cc36e4b00ab091a34
mir4ri <leobuchko@gmail.com>
2021-01-18 14:30:34 +0200

    add slider, fix opening news, add social repost, add page students and update information

---
+++ _next/static/webpack/styles.88d2fe57e036bfc4420f.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/Accordion/style.scss":
+false,
+
+/***/ "./components/NavButton/style.scss":
+false,
+
+/***/ "./pages/students/student.scss":
+false,
+
+/***/ 10:
+false,
+
+/***/ 11:
+false,
+
+/***/ 12:
+false
+
+})
---
+++ _next/static/webpack/styles.fabafde1db9bd045775a.hot-update.js
@@ -0,0 +1,69 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/Card/style.scss":
+false,
+
+/***/ "./components/News/news.scss":
+false,
+
+/***/ "./components/Tabs/tabs.scss":
+false,
+
+/***/ "./pages/entrants/preparatoryCourses/style.scss":
+false,
+
+/***/ "./pages/entrants/style.scss":
+false,
+
+/***/ "./pages/index/index.scss":
+false,
+
+/***/ "./pages/index/slider.scss":
+false,
+
+/***/ "./pages/lecturers/style.scss":
+false,
+
+/***/ "./pages/schedule/components/Lecturer/lecturer.scss":
+false,
+
+/***/ "./pages/schedule/components/Students/students.scss":
+false,
+
+/***/ "./pages/schedule/schedule.scss":
+false,
+
+/***/ 12:
+false,
+
+/***/ 13:
+false,
+
+/***/ 14:
+false,
+
+/***/ 15:
+false,
+
+/***/ 18:
+false,
+
+/***/ 19:
+false,
+
+/***/ 20:
+false,
+
+/***/ 21:
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false,
+
+/***/ 8:
+false
+
+})
