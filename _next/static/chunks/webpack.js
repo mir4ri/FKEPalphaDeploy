9b7a74e5d7d0ba4eb3cb0a3baa4cd76f527cb073
mir4ri <leobuchko@gmail.com>
2021-01-22 16:31:49 +0200

    fix

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "11660344d9fea7fbe9cd";
+/******/ 	var hotCurrentHash = "17eb73fc87397dc2fe4f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
