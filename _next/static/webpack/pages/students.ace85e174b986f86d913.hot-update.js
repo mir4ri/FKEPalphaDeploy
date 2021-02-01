a75465089725b18826dd8b12bddffa80b9325edd
mir4ri <leobuchko@gmail.com>
2021-02-02 00:10:52 +0200

    add mdx typet for news, add new documents

---
+++ _next/static/webpack/pages/students.ace85e174b986f86d913.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/students",{
+
+/***/ "./api/students.json":
+/*!***************************!*\
+  !*** ./api/students.json ***!
+  \***************************/
+/*! exports provided: 0, 1, 2, 3, 4, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"[{\\\"title\\\":\\\"Зі спеціальності 072 «Фінанси, банківська справа та страхування»\\\",\\\"document\\\":\\\"https://drive.google.com/file/d/1nfj1DeAE1BGHD4M-mdtdr_CcY9WuER0p/preview\\\"},{\\\"title\\\":\\\"Зі спеціальності 076 «Фінанси, банківська справа та страхування»\\\",\\\"document\\\":\\\"https://drive.google.com/file/d/1KbATl8lnMiyzSj3VWfCyoe52NrhST196/preview\\\"},{\\\"title\\\":\\\"Зі спеціальності 123 «Комп'ютерна інженерія»\\\",\\\"document\\\":\\\"https://drive.google.com/file/d/1IiUzR9ppLrVIt_XQwXuF3KI6UxiPQpXs/preview\\\"},{\\\"title\\\":\\\"Зі спеціальності 131 «Прикладна механіка»\\\",\\\"document\\\":\\\"https://drive.google.com/file/d/1rueVzU4LguYPFBvuUOBN3SWHfq_zc1II/preview\\\"},{\\\"title\\\":\\\"Методичні вказівки до курсового та дипломного проєктування\\\",\\\"document\\\":\\\"https://drive.google.com/file/d/1p1jQCG5LeI-cCKT9RVqPxcgvsO4o6_6D/preview\\\"}]\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2FwaS9zdHVkZW50cy5qc29uLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./api/students.json\n");
+
+/***/ })
+
+})
