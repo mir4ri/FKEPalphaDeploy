09b687c6f0dd09cbcef05ae632136cd804182d07
mir4ri <leobuchko@gmail.com>
2021-02-19 04:24:16 +0200

    add new slides, btn back in post and some info for entrants

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a5ddbb48986f4ed511a6";
+/******/ 	var hotCurrentHash = "be40336deea1517a6c50";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
