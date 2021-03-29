4bb1251206e5dedd5240e4f203dc4e0e0a13b1f4
mir4ri <leobuchko@gmail.com>
2021-03-30 02:42:24 +0300

    change header, remove bg from slider and etc

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4d58d04e6c43659e9dda";
+/******/ 	var hotCurrentHash = "ca0805a844891514784d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
