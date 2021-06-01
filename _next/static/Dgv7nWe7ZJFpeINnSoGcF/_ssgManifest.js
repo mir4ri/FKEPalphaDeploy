c7cb737747d2e9f832ce700513218eb5334c2025
mir4ri <leobuchko@gmail.com>
2021-06-01 14:00:51 +0300

    add entrants

---
+++ _next/static/Dgv7nWe7ZJFpeINnSoGcF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout\u002Fcommissions","\u002Fabout\u002Fdepartment","\u002Fentrants","\u002F","\u002Fentrants\u002F[specialty]","\u002Fabout\u002Fdepartment\u002F[department]","\u002Fabout\u002Fcommissions\u002F[commission]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
