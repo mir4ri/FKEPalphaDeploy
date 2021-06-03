17ebba61c54c9c560b5eada010446b43044cdf6e
mir4ri <leobuchko@gmail.com>
2021-06-03 10:38:35 +0300

    add dynamic posts and sort public information

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fda511be56f60199c263";
+/******/ 	var hotCurrentHash = "2723219d22fcfcf4a619";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
